038c09c1fd938a431579b62ba0108ac94a055f06
stugceates <stugceates@gmail.com>
2023-04-19 10:08:30 +0100

    add theme change and display forecast using the api

--- src/main.js
+++ src/main.js
@@ -14,10 +14,11 @@ const locationButtonElement = document.querySelector("#location-button");
 locationButtonElement.addEventListener("click", getLocation);
 
 getLocation();
-
+changeTheme();
 function getLocation (){
   navigator.geolocation.getCurrentPosition(showPosition);
 }
+
 
 function formatDate(timestamp){
   const now = new Date(timestamp*1000);
@@ -35,8 +36,43 @@ function formatDate(timestamp){
   const hours = now.getHours().toString().padStart(2, "0");
   const minutes = now.getMinutes().toString().padStart(2, "0");
   const formattedDate = `${day}, ${hours}:${minutes}`
-  return formattedDate
+  return formattedDate;
 }
+function formattedDay(timestamp){
+  const date = new Date(timestamp*1000);
+  const day = date.getDay();
+  console.log(day);
+  const days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ];
+  return days[day];
+}
+
+function changeTheme() {
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (hour >= 6 && hour < 18) {
+    document.querySelector(".weather-app").classList.add("day-bg");
+  } else {
+    document.querySelector(".weather-app").classList.add("night-bg");
+  
+  }
+}
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=fa2f0ab0044e0f6ed0fo3e30511f6tbc&units=metric` ;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 
 function displayData(response) {
   document.querySelector("#description").innerHTML = response.data.condition.description;
@@ -50,6 +86,8 @@ function displayData(response) {
   document.querySelector("#temp-value").innerHTML = temp;
 
   document.querySelector("h1.city-name").innerHTML = response.data.city;
+  
+  getForecast(response.data.coordinates);
 }
 
 function submitForm(event) {
@@ -81,7 +119,26 @@ function showPosition(position){
   axios.get(url).then(displayData); 
 }
 
+function displayForecast(response){
+  let forecast = response.data.daily;
 
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row justify-content-center">`;
+  forecast.forEach(function(forescastDay, index){
+    if (index < 5){
+    forecastHTML = 
+      forecastHTML + 
+      `<div class="col-md-2 border forecast">
+        <div class="forecast-day">${formattedDay(forescastDay.time)}</div>
+        <div class="weather-emoji"><img src="${forescastDay.condition.icon_url}"/></div>
+        <div class="forecast-temperature">${Math.round(forescastDay.temperature.day)}</div>
+      </div>`;
+  }
+});
+  forecastHTML = forecastHTML + `</div>` ;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 
 
